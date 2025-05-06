a5d33107cc0006abf460f0210d886ab040c79a82
ashleyives <ashley.ives@pnnl.gov>
2025-05-06 12:52:46 -0700

    remove extra annotations

--- FigureS#_RSD.R
+++ FigureS#_RSD.R
@@ -4,54 +4,6 @@ library(MSnSet.utils)
 library(proteomicsCV)
 library(tidyverse)
 
-#not log transformed data 
-
-# nm = load("msnset_humanislet_int_notnormalized.RData")
-# 
-# mcontrol <- m[,(str_detect(sampleNames(m), "Nottreated"))] 
-#   
-# mtreatment <- m[,(str_detect(sampleNames(m), "Treated"))] 
-# 
-# cvscontrol <- protCV(data.frame(exprs(mcontrol))) %>%
-#   data.frame() %>%
-#   mutate(Treatment = "Control") %>%
-#   mutate(Normalization = "Unnormalized")
-# 
-# cvstreatment <- protCV(data.frame(exprs(mtreatment))) %>%
-#   data.frame() %>%
-#   mutate(Treatment = "Treated")%>%
-#   mutate(Normalization = "Unnormalized")
-# 
-# norm_coeffs <- apply(exprs(m), 2, median, na.rm = T) #calculates the median of each column in the expression matrix while ignoring any NA values.
-# exprs(m) <- sweep(exprs(m), 2, norm_coeffs, FUN = "/") #applies normalization to msnset via sweep
-# 
-# mcontrol <- m[,(str_detect(sampleNames(m), "Nottreated"))] 
-# 
-# mtreatment <- m[,(str_detect(sampleNames(m), "Treated"))] 
-# 
-# cvscontrol2 <- protCV(data.frame(exprs(mcontrol))) %>%
-#   data.frame() %>%
-#   mutate(Treatment = "Control") %>%
-#   mutate(Normalization = "Median normalized")
-# 
-# cvstreatment2 <- protCV(data.frame(exprs(mtreatment))) %>%
-#   data.frame() %>%
-#   mutate(Treatment = "Treated")%>%
-#   mutate(Normalization = "Median normalized")
-# 
-# plot_data <- rbind(cvscontrol, cvstreatment, cvscontrol2, cvstreatment2)
-# 
-# ggplot(plot_data, aes(x = Treatment, y = ., fill = Treatment)) +
-#   geom_violin() +
-#   stat_summary(fun.data = function(y) data.frame(y = median(y), label = paste0("Median: ", round(median(y), 2))),
-#                geom = "point", shape = 21, size = 2, color = "black", fill = "red") +
-#   stat_summary(fun.data = function(y) data.frame(y = median(y), label = round(median(y), 2)),
-#                geom = "text", vjust = -0.5) +
-#   labs(x = "Treatment", y = "RSD %") +
-#   theme_bw(base_size = 24)+
-#   facet_wrap(~Normalization)
-
-#trying RSD via James method, distribution looks the same 
 nm = load("msnset_humanislet_int_notnormalized.RData")
 
 exprs(m) <- log2(exprs(m))
