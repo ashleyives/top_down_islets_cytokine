c978086c381ff31ac39dd20051329ef56853d202
ashleyives <ashley.ives@pnnl.gov>
2025-07-28 10:40:46 -0700

    add annotations

--- Figure4_CXCL.R
+++ Figure4_CXCL.R
@@ -1,8 +1,13 @@
+# Authors: Ashley Ives
+# Purpose: Takes output of script "0a" and generates Figure 4: Heatmap and table summary of observed CXCL proteoforms. 
+# Performs hypergeometric probability distribution test to determine which unique observations are statistically significant.
 
 library(tidyverse)
 library(Seurat)
 library(viridis)
 library(gt)
+
+# 1. Load data generated in "0a_loading_intensity_data.R" and convert to usable format.  
 
 load("msnset_humanislet_int_notnormalized.RData")
 
@@ -39,6 +44,8 @@ x <- exprs(m) %>%
 SampleID_meta <- x %>%
   distinct(SampleID, Treatment, Pair)
 
+# 2. Perform hypergeometric probability distribution----------------------------
+
 uniquePFs <- x %>%
   filter(!is.na(Intensity)) %>%
   group_by(PF) %>%
@@ -63,32 +70,13 @@ meta <- fData(m) %>%
   dplyr::select(firstAA, lastAA, PF, Proteoform, Gene, UniProtAcc)%>%
   mutate(isSig = ifelse(PF %in% uniquePFs$PF, TRUE, FALSE))
 
-#original plot 
-# plot <- x %>%
-#   filter(PF %in% uniquePFs$PF) %>%
-#   mutate(Sample = paste(Treatment, Pair, sep  = "_")) %>%
-#   # left_join(meta) %>%
-#   ggplot()+
-#   aes(x = Sample, y = fct_reorder(PF, Intensity), fill = log2(Intensity))+
-#   geom_tile()+
-#   scale_fill_viridis(na.value = "gray") + 
-#   # scale_fill_distiller(palette = "RdPu",
-#   #                      na.value = "black")+
-#   theme_minimal(base_size = 12)+
-#   theme(axis.text.y=element_text(color = 'black', size = 12),
-#                 axis.text.x=element_text(angle = 90, vjust = 0.5, hjust= 0.5, color='black',size = 12))+
-#   xlab("")+
-#   ylab("")
-# plot
-
-##########visualize all CXCL observations 
-
-#identifier for patients 
 pair_mapping <- c('Pair1' = 'A', 'Pair2' = 'B', 'Pair3' = 'C', 'Pair5' = 'D', 'Pair6' = 'E', 'Pair7' = 'F')
 
 # Define the specific order for PF values
 specific_order <- c("CXCL11_2", "CXCL11_1", "CXCL10_8", "CXCL10_7", "CXCL10_6", 
                     "CXCL10_4", "CXCL10_3", "CXCL10_2", "CXCL9_1",  "CXCL1_1")
+
+# 3. Generate Figure 4. Heatmap of CXCL proteoforms.---------------------------- 
 
 plot <- x %>%
   filter(grepl("CXCL", PF)) %>%
@@ -144,25 +132,3 @@ ggsave(plot = combplot, filename= "FigureS_CXCL.png", scale=1.5,
        height = 170,
        dpi = 800,
        units = c("mm"))
-
-#which PFRs are significant by hypergeometric distribution 
-uniquePF_Pair <- x %>%
-  filter(!is.na(Intensity)) %>%
-  group_by(PF) %>%
-  add_count(name = "TotalN") %>%
-  group_by(Pair, PF) %>%
-  add_count(name = "GroupN") %>%
-  ungroup() %>%
-  group_by(Pair) %>%
-  mutate(GroupSize = max(GroupN)) %>%
-  filter(GroupSize == GroupN) %>%
-  mutate(Check1 = TotalN - GroupSize) %>%
-  ungroup() %>%
-  mutate(Prob0 = (factorial(nrow(SampleID_meta)-GroupSize))/(factorial(Check1)*factorial((nrow(SampleID_meta)-GroupSize)-Check1))) %>%
-  mutate(Prob1 = (factorial(nrow(SampleID_meta)))/(factorial(TotalN)*factorial(nrow(SampleID_meta)-TotalN))) %>%
-  mutate(Probability = Prob0/Prob1) %>%
-  group_by(Pair, PF, Probability) %>%
-  summarize(Avg = mean(Intensity))  %>%
-  ungroup() %>%
-  filter(Probability < 0.01)
-
